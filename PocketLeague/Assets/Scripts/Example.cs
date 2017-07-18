3c4591bdeda8129bb83eb2454c88b921e573c20d
Mark Bouwman <mark.bouwman@mediamonks.com>
2017-07-18 14:32:46 +0200

    Added example

---
+++ PocketLeague/Assets/Scripts/Example.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using RLSApi;
+using RLSApi.Models;
+
+public class Example : MonoBehaviour {
+	void Awake() {
+		RLSClient.GetTiers(null, null);
+	}
+
+	private void OnGetTiers(Tier[] tiers) {
+		Debug.Log("Got: " + tiers.Length + " tiers.");
+		foreach(var t in tiers) {
+			Debug.Log(t.tierName);
+		}
+	}
+
+	private void OnGetTiersFail(Error error) {
+		Debug.Log("Got error with code: " + error.Code);
+		Debug.Log(error.Message);
+	}
+}

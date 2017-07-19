605ad9d54b0baf0e7150800de4de14b49255b4fe
Mark Bouwman <mark.bouwman@mediamonks.com>
2017-07-19 14:36:29 +0200

    Added rank visuals and updated menu

--- PocketLeague/Assets/Scripts/Example.cs
+++ PocketLeague/Assets/Scripts/Example.cs
@@ -5,13 +5,19 @@ using RLSApi.Data;
 
 public class Example : MonoBehaviour {
 	void Awake() {
-		RLSClient.GetPlayers(new[] {
-				new PlayerBatchRequest(RlsPlatform.Steam, "76561198033338223"),
-				new PlayerBatchRequest(RlsPlatform.Ps4, "Wizwonk"),
-				new PlayerBatchRequest(RlsPlatform.Xbox, "Loubleezy")
-	}, (data) => {
-		Debug.Log(data[0].DisplayName);
-		Debug.Log(data[1].DisplayName);
-	}, null);
+		RLSClient.GetLeaderboardRanked(RlsPlaylistRanked.Doubles, (data) => {
+			foreach(var v in data) {
+				Debug.Log(v.DisplayName);
+			}
+		}, null);
+
+		/*RLSClient.GetPlayers(new[] {
+					new PlayerBatchRequest(RlsPlatform.Steam, "76561198033338223"),
+					new PlayerBatchRequest(RlsPlatform.Ps4, "Wizwonk"),
+					new PlayerBatchRequest(RlsPlatform.Xbox, "Loubleezy")
+		}, (data) => {
+			Debug.Log(data[0].DisplayName);
+			Debug.Log(data[1].DisplayName);
+		}, null);*/
 	}
 }
